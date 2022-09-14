cdb285eef43c555b4091ef2c848a192bb3e3faf2
Camilo Rodríguez <camilo@sdkconsultoria.com>
2022-09-13 19:19:12 -0500

    Refactor ResumableUploadapi

--- src/Services/ResumableUploadAPI.php
+++ src/Services/ResumableUploadAPI.php
@@ -7,41 +7,56 @@ use Illuminate\Support\Facades\Http;
 
 class ResumableUploadAPI extends FacebookService
 {
+    protected $mime_type;
+    protected $file_size;
+    protected $file;
+    protected $session_id;
+    protected $handler;
+
     public static function uploadFile(string $file)
     {
+        $object = new self($file);
+        $object->createUploadSession();
+        $object->uploadOrFail();
+
+        return $object;
+    }
+
+    function __construct(string $file) {
         if (file_exists($file)) {
-            $object = new self();
-            $session = $object->createUploadSessionAndReturn($file);
-            return $object->initiateUploadoRfAIL($file, $session['id']);
+            $this->file = $file;
+            $this->mime_type = mime_content_type($file);
+            $this->file_size = filesize($file);
         } else {
             throw new Exception("El archivo {$file} no existe.");
         }
     }
 
-    private function createUploadSessionAndReturn(string $file)
+    private function createUploadSession(): void
     {
         $url = $this->graph_url . config('facebook.app_id') . "/uploads";
-        $url .= '?file_length='.filesize($file);
-        $url .= '&file_type='.mime_content_type($file);
+        $url .= '?file_length='.$this->file_size;
+        $url .= '&file_type='.$this->mime_type;
         $url .= '&access_token='.config('facebook.app_token');
 
-        return Http::post($url)->throw()->json();
+        $response = Http::post($url)->throw()->json();
+        $this->session_id = $response['id'];
     }
 
-    private function initiateUploadoRfAIL(string $file, string $session_id, string $offset = '0')
+    private function uploadOrFail(string $offset = '0')
     {
-        $url = $this->graph_url . $session_id;
+        $url = $this->graph_url . $this->session_id;
 
         $response = Http::withHeaders([
             'Authorization' => 'OAuth '. config('facebook.app_token'),
             'file_offset' => $offset
-        ])->withBody(file_get_contents($file), mime_content_type($file))
+        ])->withBody(file_get_contents($this->file), $this->mime_type)
         ->post($url)->throw()->json();
 
-        if (isset($response['h'])) {
-            return $response;
+        if (!isset($response['h'])) {
+            throw new Exception(json_encode($response));
         }
 
-        throw new Exception(json_encode($response));
+        $this->handler = $response['h'];
     }
 }
