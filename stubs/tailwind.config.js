2ad31060b2528fceb9541d0b0936a2349c165de7
Camilo Rodriguez <hellsythex@gmail.com>
2024-02-15 12:58:57 -0600

    Added view and update tailwindconfig

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/components/**/*.vue',
     ],
 
     theme: {
