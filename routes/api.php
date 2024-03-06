4c52648efaabf9c436fd6ceb718b6e39cd744763
Camilo Rodriguez <hellsythex@gmail.com>
2024-03-06 17:51:28 -0600

    Fix routes compatibiliti

--- routes/api.php
+++ routes/api.php
@@ -13,29 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$routes = function () {
+    Route::get('whatsapp-webhook', 'WebhookController@subscribe')->name('meta.webhook.subscribe');
+    Route::post('whatsapp-webhook', 'WebhookController@webhook')->name('meta.webhook');
+
+    Route::get('waba/{wabaId}/init', 'WabaController@init')->name('waba.init');
+    Route::get('waba/{wabaId}/get-templates', 'WabaController@loadTemplatesFromWaba')->name('waba.getTemplatesFromMeta');
+    Route::get('waba/{wabaId}/get-info', 'WabaController@getWabaInfoFromMeta')->name('waba.getInfoFromMeta');
+    Route::get('waba/{wabaId}/get-phones', 'WabaController@getWabaPhonesFromMeta')->name('waba.getPhonesFromMeta');
+
+    Route::get('template', 'TemplateController@index')->name('template.index');
+    Route::post('template', 'TemplateController@storage')->name('template.storage');
+    Route::put('template/{id}', 'TemplateController@edit')->name('template.edit');
+    Route::delete('template/{id}', 'TemplateController@delete')->name('template.delete');
+
+    Route::post('message/send', 'MessageController@sendMessage')->name('message.send');
+    Route::post('message/template/send', 'MessageController@sendTemplate')->name('message.template.send');
+    Route::get('message', 'MessageController@index')->name('message.index');
+
+    Route::get('waba-phone', 'WabaPhoneController@index')->name('waba.waba_number');
+
+    Route::get('chat', 'ChatController@index')->name('chat.index');
+
+    Route::post('campaign', 'CampaignController@store')->name('campaign.store');
+};
+
 Route::namespace('Sdkconsultoria\WhatsappCloudApi\Http\Controllers')
     ->prefix('api/v1')
-    ->group(function () {
-        Route::get('whatsapp-webhook', 'WebhookController@subscribe')->name('meta.webhook.subscribe');
-        Route::post('whatsapp-webhook', 'WebhookController@webhook')->name('meta.webhook');
+    ->group($routes);
 
-        Route::get('waba/{wabaId}/init', 'WabaController@init')->name('waba.init');
-        Route::get('waba/{wabaId}/get-templates', 'WabaController@loadTemplatesFromWaba')->name('waba.getTemplatesFromMeta');
-        Route::get('waba/{wabaId}/get-info', 'WabaController@getWabaInfoFromMeta')->name('waba.getInfoFromMeta');
-        Route::get('waba/{wabaId}/get-phones', 'WabaController@getWabaPhonesFromMeta')->name('waba.getPhonesFromMeta');
-
-        Route::get('template', 'TemplateController@index')->name('template.index');
-        Route::post('template', 'TemplateController@storage')->name('template.storage');
-        Route::put('template/{id}', 'TemplateController@edit')->name('template.edit');
-        Route::delete('template/{id}', 'TemplateController@delete')->name('template.delete');
-
-        Route::post('message/send', 'MessageController@sendMessage')->name('message.send');
-        Route::post('message/template/send', 'MessageController@sendTemplate')->name('message.template.send');
-        Route::get('message', 'MessageController@index')->name('message.index');
-
-        Route::get('waba-phone', 'WabaPhoneController@index')->name('waba.waba_number');
-
-        Route::get('chat', 'ChatController@index')->name('chat.index');
-
-        Route::post('campaign', 'CampaignController@store')->name('campaign.store');
-    });
+Route::namespace('Sdkconsultoria\WhatsappCloudApi\Http\Controllers')
+    ->group($routes);
